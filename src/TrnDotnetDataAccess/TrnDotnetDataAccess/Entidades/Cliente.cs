d6388e0ebe8e12dce27c02bf77507827f7c20568
lano.castro <lano.castro@cg.df.gov.br>
2021-03-24 14:31:17 -0300

    initial commit

---
+++ src/TrnDotnetDataAccess/TrnDotnetDataAccess/Entidades/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrnDotnetDataAccess.Entidades
+{
+    public class Cliente
+    {
+       
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Senha { get; private set; }
+        public Cliente(string nome, string email, string senha)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+        }
+
+    }
+}
---
+++ src/TrnDotnetDataAccess/TrnDotnetDataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrnDotnetDataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
